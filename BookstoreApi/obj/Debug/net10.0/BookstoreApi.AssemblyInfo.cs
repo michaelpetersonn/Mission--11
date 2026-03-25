4d6093b381da51d4dec4683eff952320384dfc36
michaelpetersonn <mpeter15@byu.edu>
2026-03-25 09:32:47 -0600

    new branch

--- BookstoreApi/obj/Debug/net10.0/BookstoreApi.AssemblyInfo.cs
+++ BookstoreApi/obj/Debug/net10.0/BookstoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0e0fc8e0a182964db639b9f84391988a26a399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47621e76bc7eb82cb2a0afb52763c5b5a9750e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
